be92f7663b70cafb07054042f01625a6c4b9a355
James Kim (Daewon) <eodnjs2998@gmail.com>
2023-02-15 15:15:58 +0900

    Update PostBuild_AddStickers.cs

--- Playables.Stickers.Editor.iOS/PostBuild_AddStickers.cs
+++ Playables.Stickers.Editor.iOS/PostBuild_AddStickers.cs
@@ -35,6 +35,9 @@ public class PostBuild_AddStickers : IPostprocessBuildWithReport
 		PBXProject pbxProject = new PBXProject();
 		pbxProject.ReadFromFile(pbxPath);
 
+		if (pbxProject.TargetGuidByName("Stickers") != null)
+			return;
+
 		string appGuid = pbxProject.GetUnityMainTargetGuid();
 
 		string stickersGuid = pbxProject.AddStickerExtension(appGuid, "Stickers",  PlayerSettings.applicationIdentifier+".Stickers", "Stickers/Info.plist");
@@ -42,14 +45,15 @@ public class PostBuild_AddStickers : IPostprocessBuildWithReport
 		pbxProject.SetTeamId(stickersGuid,PlayerSettings.iOS.appleDeveloperTeamID);
 		
 		var stickersInfoGuid = pbxProject.AddFile("Stickers/Info.plist", "Stickers/Info.plist");
-		
+
 		var stickersAssetGuid = pbxProject.AddFile("Stickers/Stickers.xcassets", "Stickers/Stickers.xcassets");
 		pbxProject.AddFileToBuild(stickersGuid, stickersAssetGuid);
 
-		pbxProject.SetBuildProperty(stickersGuid, "IPHONEOS_DEPLOYMENT_TARGET", "10.0");
+		pbxProject.SetBuildProperty(stickersGuid, "IPHONEOS_DEPLOYMENT_TARGET", "16.2");
 		pbxProject.AddBuildProperty(stickersGuid, "TARGETED_DEVICE_FAMILY", "1,2");
 		pbxProject.AddBuildProperty(stickersGuid, "ASSETCATALOG_COMPILER_APPICON_NAME", "iMessage App Icon");
 		pbxProject.AddBuildProperty(stickersGuid, "ALWAYS_SEARCH_USER_PATHS", "NO");
+		pbxProject.AddBuildProperty(stickersGuid, "GENERATE_INFOPLIST_FILE", "YES");
 
 		pbxProject.WriteToFile(pbxPath);
 	}
